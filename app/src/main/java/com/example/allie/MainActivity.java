85c6eb094df2fe0da876fc860e13367440bbfa97
albearli <yiliang6@illinois.edu>
2020-01-17 23:12:12 -0600

    Play with canvas tools

--- app/src/main/java/com/example/allie/MainActivity.java
+++ app/src/main/java/com/example/allie/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.allie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.LinearLayout;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        linearLayout = findViewById(R.id.linearLayout);
+        CanvasTest canvas = new CanvasTest(this);
+        linearLayout.addView(canvas);
     }
+//    private Canvas mcanvas = new Canvas();
+//    private Paint mpaint = new Paint();
+//    private Bitmap
 }
