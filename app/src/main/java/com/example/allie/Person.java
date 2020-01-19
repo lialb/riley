068363b249abfe1e5ea45eb21c2fba6bde4356a4
Theodore Li <theodoreqili@gmail.com>
2020-01-18 20:38:31 -0600

    Add spretsheet animation

--- app/src/main/java/com/example/allie/Person.java
+++ app/src/main/java/com/example/allie/Person.java
@@ -11,6 +11,7 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.AnimatedVectorDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
 
 import androidx.core.content.res.ResourcesCompat;
 
@@ -20,7 +21,7 @@ public class Person {
     private Rect rect;
     private Paint paint = new Paint();
     private Arm rightArm = new Arm();
-    private AnimatedVectorDrawable person;
+    private VectorDrawable person;
 
     Person(int x, int y, Context context) {
         rect = new Rect(x, y, x + 100, y + 100);
@@ -29,7 +30,7 @@ public class Person {
         paint.setColor(Color.BLACK);
         paint.setStyle(Paint.Style.STROKE);
 
-        person = (AnimatedVectorDrawable) ResourcesCompat.getDrawable(context.getResources(), R.drawable.test_animate, null);
+        person = (VectorDrawable) ResourcesCompat.getDrawable(context.getResources(), R.drawable.walk_cycle, null);
         person.setBounds(10, 10, 1000, 1000);
     }
 
@@ -38,7 +39,7 @@ public class Person {
     }
 
     public void start() {
-        person.start();
+//        person.start();
     }
 
     public Arm getArm() {
