02c54604fc523e3481e5e7b89452ae7f216696e7
beezow <zbeezow@gmail.com>
2020-01-19 02:14:42 -0500

    maybe a personstep class to handle moving the person similiar to background and graphic

---
+++ app/src/main/java/com/example/allie/AnimationStepImplementations/PersonStep.java
@@ -0,0 +1,30 @@
+package com.example.allie.AnimationStepImplementations;
+
+import com.example.allie.AnimationStepImplementations.AnimationStep;
+import com.example.allie.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonStep extends AnimationStep  {
+    Person person;
+    List<int[]> steps;
+
+    public PersonStep (Person person) {
+        this.person = person;
+        steps = new ArrayList<>();
+    }
+
+    public void addMove (int... move) {
+        steps.add(move);
+    }
+
+    @Override
+    public void run() {
+        person.setWalking(false);
+        for (int[] step : steps) {
+            person.move(step[0], step[1]);
+        }
+    }
+}
+
